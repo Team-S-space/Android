d323c63ad6c0f4735197cb9d9ce11e8bac946982
haein45 <ohhaein0405@gmail.com>
2025-01-12 02:47:37 +0900

    list

--- app/src/main/java/com/example/umc_7th_hackathon/list/ListActivity.kt
+++ app/src/main/java/com/example/umc_7th_hackathon/list/ListActivity.kt
@@ -18,9 +18,9 @@ class ListActivity : AppCompatActivity() {
 
         // 데이터 생성
         val itemList = ArrayList<Item>()
-        itemList.add(Item("오제이", "서울", "06:00 AM", "06:00 PM", 0))
-        itemList.add(Item("홍길동", "부산", "06:30 AM", "06:30 PM", 0))
-        itemList.add(Item("마이네임", "대구", "07:00 AM", "07:00 PM",0))
+        itemList.add(Item("전동진에서 유명한 스팟이냐구", "서울", "06:00 AM", "06:00 PM", 0))
+        itemList.add(Item("전동진에서 유명한 스팟이냐구", "부산", "06:30 AM", "06:30 PM", 0))
+        itemList.add(Item("전동진에서 유명한 스팟이냐구", "대구", "07:00 AM", "07:00 PM",0))
 
         recyclerViewAdapter = RecyclerViewAdapter(itemList)
 
